cacb9fc48b3e8815258df1fdb25c1f43f2af2946
Oscar Roberto Mollo Ajhuacho <mollo2k@gmail.com>
2018-09-14 01:11:11 -0400

    fetch

--- interfaz.js
+++ interfaz.js
@@ -1,25 +1,60 @@
-document.getElementById("breg_sw").addEventListener("click", freg_sw);
-window.addEventListener('load', fload);
 var vload=false;
+function fload() {
+	//Deshabilitar el menu contextual...
+	document.addEventListener("contextmenu", function(e) {e.preventDefault();});
 
-function freg_sw() {
+	vload = true;
+	console.log("Carga completa...");
+	if (window.matchMedia('(display-mode: standalone)').matches) {
+		vbtn_reg.value = "StandAlone";
+		vbtn_reg.style.color = "yellow";
+		console.log('Modo: standalone');
+  }else{
+		vbtn_reg.disabled = false;
+		vbtn_reg.value = "Instalar PWA como APP";
+		vbtn_reg.style.color = "green";
+		console.log('Modo: browser');
+	}
+}
+
+function freg_sw() {  //Al precionar el boton de registro.
 	if('serviceWorker' in navigator) {
-		console.log('El navegador soporta ServiceWorker');
+		console.log('Soporta ServiceWorker');
 		if(vload){
-			console.log('Se inicia el registro');
-			navigator.serviceWorker.register('./sw.js').then(reg => console.log('Registro de SW exitoso', reg))
-			.catch(err => console.warn('Al final no se registro', err))
+			console.log('Iniciar el registro...');
+			navigator.serviceWorker.register('./sw.js').then(reg => {
+				reg.installing; // Instalando...
+				reg.waiting; // Esperando
+				reg.active; // Activo.
+
+				reg.addEventListener('updatefound', () => {
+					const newWorker = reg.installing;
+					console.log(newWorker.state);
+
+					newWorker.addEventListener('statechange', () => {
+						// newWorker.state a cambiado.
+						console.log(newWorker.state);
+						if(newWorker.state == "installed") {
+							vbtn_reg.disabled = true;
+							vbtn_reg.style.backgroundColor = "orange";
+						}
+						if(newWorker.state == "activated") {
+							vbtn_reg.style.backgroundColor = "lightblue";
+						}
+					});
+				});
+			});
 		}
 	} else {
 		console.log('No soporta ServiceWorker');
 	}
 }
 
-function fload() {
-	vload = true;
-	console.log("Carga completa...");
-	if (window.matchMedia('(display-mode: standalone)').matches) {
-		console.log('Modo standalone');
-  }
+var vbtn_reg = document.getElementById("btn_reg_sw");
+vbtn_reg.addEventListener("click", freg_sw);
+window.addEventListener('load', fload);
 
-}
+// ONLINE - OFFLINE
+setInterval(function () {
+  vbtn_reg.style.borderColor = navigator.onLine ? 'green' : 'red';
+}, 250);
--- sw.js
+++ sw.js
@@ -1,42 +1,38 @@
 //Asignar un nombre y version al cache
-const CACHE_NAME = 'v1';
+const CACHE_NAME = 'v:1.0';
 urlsToCache = ['./', './estilo.css', './interfaz.js', './img/favicon.png'];
 
-//Al instalar...
+//Al instalar. Especificar cache...
 self.addEventListener('install', e => {
+  console.log("Evento: install");
   e.waitUntil(caches.open(CACHE_NAME)
     .then(function(cache) {
       return cache.addAll(urlsToCache);
     })
-    .catch(err => console.log('Error en registro de cache', err))
+    .catch(err => console.log('Error: registro de cache', err))
   );
 });
-  
+// Activado...
 self.addEventListener('activate', e => {
    const cacheWhitelist = [CACHE_NAME];
-   
+   console.log("Evento: activate");
    e.waitUntil(caches.keys()
    .then(cacheNames => {
      cacheNames.map(key => {
        if(cacheWhitelist.indexOf(key) === -1){
-         return caches.delete(key)
+         return caches.delete(key);
        }
-     })
+     });
     })
-     
-    .then(() => self.clients.claim())
+      .then(() => self.clients.claim())
   );
 });
 
-//CUANDO SE RECUPERA UNA URL
+//[F5] networkFirst
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request)
-    .then(resp => {
-      if(resp){
-        return resp;
-    }
-    
-    return fetch(e.request);
-  })
+  console.log("Evento: fetch");
+  e.respondWith(fetch(e.request).catch(function(){
+    return caches.match(e.request);
+    })
   );
 });
